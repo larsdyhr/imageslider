f9954047ea2f10810dbca975668555203083c475
Dycon, Lars Dyhr <lars@dycon.dk>
2018-01-15 08:58:23 +0100

    [RELEASE] Release of version 1.2.0. First complete version with implemented frontend and feature complete backend.

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
   'state' => 'stable',
   'uploadfolder' => false,
   'createDirs' => '',
-  'version' => '1.1.0',
+  'version' => '1.2.0',
   'clearcacheonload' => true,
 );
 
